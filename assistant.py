6ddd7a61c2cfac982101b17bffa3e66d3e0affaa
André Miller <andreluismiller@gmail.com>
2026-07-05 04:50:21 +0000

    feat(monitor): setting up RAG Assistant

---
+++ assistant.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+from ingest import load_faq_data, build_index
+from rag_helper import RAGBase
+
+
+
+def create_assistant():
+    load_dotenv()
+    groq_client = OpenAI(
+        api_key=os.environ["GROQ_API_KEY"],
+        base_url="https://api.groq.com/openai/v1",
+    )
+    documents = load_faq_data()
+    index = build_index(documents)
+
+    return RAGBase(
+        index=index,
+        llm_client=groq_client
+    )
+
+
+if __name__ == "__main__":
+    assistant = create_assistant()
+
+    query = "Quem tem direito ao Pé-de-Meia?"
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+
+    answer = assistant.rag(query)
+    print(answer)
