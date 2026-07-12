c11d5a36d7a55966a7bae15960c9a580c5cddd58
André Miller <andreluismiller@gmail.com>
2026-07-12 18:24:21 +0000

    feat(ingest): update load_faq_data function to load data from dataset.json

--- ingest.py
+++ ingest.py
@@ -1,11 +1,20 @@
+from pathlib import Path
 import json
 from minsearch import Index
 
 def load_faq_data():
     """Carrega os dados do FAQ do MEC a partir do arquivo JSON."""
-    with open("mec_faq_enriquecido.json", encoding="utf-8") as f:
-        documents = json.load(f)
-    return documents
+    file_path = Path(__file__).parent / "data" / "raw" / "dataset.json"
+
+    if not file_path.is_file():
+        raise FileNotFoundError(f"Arquivo não encontrado: {file_path}")
+    
+    with open(file_path, encoding="utf-8") as f:
+        return json.load(f)
+    
+    # with open("mec_faq_enriquecido.json", encoding="utf-8") as f:
+    #     documents = json.load(f)
+    # return documents
 
 
 def build_index(documents):
